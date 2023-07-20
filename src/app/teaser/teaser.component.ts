ba2ef4ce186e350f516398d9ff93500cb146f1db
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-20 14:55:44 +0200

    added teaser

--- src/app/teaser/teaser.component.ts
+++ src/app/teaser/teaser.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-teaser',
   templateUrl: './teaser.component.html',
   styleUrls: ['./teaser.component.scss']
 })
-export class TeaserComponent {
+export class TeaserComponent implements OnInit {
   subline = [
     'The web is your stage.',
 		'Code is your brush.',
-		'Design your stage!'
-  ]
+		'Design your stage!',
+    '',
+  ];
+    currentSub = 0;
+    showSubline = true;
+
+    ngOnInit() {
+      this.updateImage();
+    }
+  
+    updateImage() {
+      setInterval(() => {
+        this.currentSub++;
+        this.currentSub = this.currentSub % this.subline.length;
+        this.showSubline = false;
+        setTimeout(() => {
+          this.showSubline = true;
+        }, 10);
+      }, 5000)
+    }
 }
