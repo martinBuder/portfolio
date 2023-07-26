d8fa25fa1b8e08e5a072e1a8b2c6f035262a553b
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-26 11:44:34 +0200

    work on portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,36 +8,28 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
   projects = [
     {
+      img: 'pokedex',
+      weblink: 'https://designyourstage.com/pokedex/',
+      title: 'Pokedex',
+      description: 'Based on the PokèAPI. A sipmle libary that provides and catalouges pokemon information.',
+      codeElements: 'JavaScript | API | HTML | CSS',
+      githubLink: 'https://github.com/martinBuder/pokemon',
+    },
+    {
+      img: 'join',
+      weblink: 'https://designyourstage.com/Join/',
+      title: 'Join',
+      description: 'Task Manager inspired by the Kanban System. Create and organize tasks usin drag and drop functions, assigns users and categories. Datas saved by using local and remote storage', 
+      codeElements: 'JavaScript | HTML | CSS',
+      githubLink: 'https://github.com/martinBuder/join',
+    },
+    {
       img: 'sharkiegame',
-      weblink: '',
+      weblink: 'https://designyourstage.com/sharkieGame/',
       title: 'Sharkie',
       description: 'A simple Jump-and-Run game based on an object-oriented apporach. Help sharkie to find Coins and Poisens to fight against enemies and the final killer whale.',
       codeElements: 'JavaScript(OOP) | HTML | CSS',
-      githubLink: 'string',
+      githubLink: 'https://github.com/martinBuder/SharpieGame',
     },
-    {
-      img: 'sharkiegame',
-      weblink: '',
-      title: 'Sharkie',
-      description: 'string',
-      codeElements: 'string',
-      githubLink: 'string',
-    },
-    {
-      img: 'sharkiegame',
-      weblink: '',
-      title: 'Sharkie',
-      description: 'string',
-      codeElements: 'string',
-      githubLink: 'string',
-    },
-    {
-      img: 'sharkiegame',
-      weblink: '',
-      title: 'Sharkie',
-      description: 'string',
-      codeElements: 'string',
-      githubLink: 'string',
-    },
-  ]
+  ] //img = 4/3
 }
